72e241bfde72c37d7de34c7d2936d017e8784178
1Basco <lsos13@gmail.com>
2023-07-14 17:55:05 -0300

    [Fix - FormatDate](Fix format type)

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,10 @@
 export const formatDateToLongMonth = (originalDate: string) => {
   const date = new Date(originalDate);
-  const options = { year: "numeric", month: "long", day: "numeric" };
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 
   return date.toLocaleDateString("en-US", options);
 };
